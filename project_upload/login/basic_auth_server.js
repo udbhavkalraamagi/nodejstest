1d51ace214cdc764eb3f2e9a3100ce9edfe1e3f7
udbhav <udbhav.kalra@amagi.com>
2017-09-26 08:51:06 +0530

    login and upload

---
+++ project_upload/login/basic_auth_server.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/node
+
+var http = require('http');
+var auth = require('http-auth');
+
+// Authentication
+var basic = auth.basic({
+	realm: "User Account",
+	file: __dirname + "/htpasswd"
+});
+
+// Welcome
+http.createServer(basic, (req, res) => {
+	res.end(`Welcome - ${req.user}!`);
+})
+.listen(3000, () => {
+	console.log("Server running at http://localhost:3000/");
+});
+
