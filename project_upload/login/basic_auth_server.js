b967a9e5ead56516cb4cf475be5a2c202be3b84c
udbhavkalraamagi <30718008+udbhavkalraamagi@users.noreply.github.com>
2017-09-26 08:58:47 +0530

    login and upload

--- project_upload/login/basic_auth_server.js
+++ project_upload/login/basic_auth_server.js
@@ -5,15 +5,15 @@ var auth = require('http-auth');
 
 // Authentication
 var basic = auth.basic({
-	realm: "User Account",
-	file: __dirname + "/htpasswd"
+  realm: "User Account",
+  file: __dirname + "/htpasswd"
 });
 
 // Welcome
 http.createServer(basic, (req, res) => {
-	res.end(`Welcome - ${req.user}!`);
+  res.end(`Welcome - ${req.user}!`);
 })
 .listen(3000, () => {
-	console.log("Server running at http://localhost:3000/");
+  console.log("Server running at http://localhost:3000/");
 });
 
