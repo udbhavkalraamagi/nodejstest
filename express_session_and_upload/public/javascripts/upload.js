3f98db20f36b0912183792837558828b9c8e17ba
udbhav <udbhav.kalra@amagi.com>
2017-10-03 17:48:40 +0530

    render upload refactor

--- express_session_and_upload/public/javascripts/upload.js
+++ express_session_and_upload/public/javascripts/upload.js
@@ -1,49 +1,43 @@
 
 function uploadnow(){
-    var files = $('#upload-input').get(0).files;
+  let files = $('#upload-input').get(0).files;
 
-    if (files.length >= 1){
+  if (files.length >= 1){
+    let formData = new FormData();
 
-      var formData = new FormData();
+    for(let i = 0; i < files.length; i++) {
+      let file = files[i];
+      formData.append('uploads[]', file, file.name);
+    }
 
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        formData.append('uploads[]', file, file.name);
-      }
+    $.ajax({
+      url: '/upload',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      
+      success: function(data){
+        console.log('upload successful!\n' + data);
+      },
+      xhr: function() {
+        let xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener('progress', function(evt) {
+          if (evt.lengthComputable) {
+            let percentComplete = evt.loaded / evt.total;
+            percentComplete = parseInt(percentComplete * 100);
+            $('.progress-bar').text(percentComplete + '%');
+            $('.progress-bar').width(percentComplete + '%');
 
-      $.ajax({
-        url: '/upload',
-        type: 'POST',
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(data){
-            console.log('upload successful!\n' + data);
-        },
-        xhr: function() {
-          var xhr = new XMLHttpRequest();
-
-          xhr.upload.addEventListener('progress', function(evt) {
-
-            if (evt.lengthComputable) {
-              var percentComplete = evt.loaded / evt.total;
-              percentComplete = parseInt(percentComplete * 100);
-
-              $('.progress-bar').text(percentComplete + '%');
-              $('.progress-bar').width(percentComplete + '%');
-
-              if (percentComplete === 100) {
-
-                $('.progress-bar').html('Following files are uploaded !');
-              }
-
+            if (percentComplete === 100) {
+              $('.progress-bar').html('Following files are uploaded !');
             }
 
-          }, false);
-          
-          return xhr;
+          }
+        }, false);
+
+        return xhr;
         }
       });
-
-    }
-}
+    }//end of 'if'
+} // end of function
