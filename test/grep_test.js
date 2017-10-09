f688356b7a362d130d9b587a9232438b9204bcb7
udbhav <udbhav.kalra@amagi.com>
2017-10-09 18:37:16 +0530

    adding cases grep.js

--- test/grep_test.js
+++ test/grep_test.js
@@ -42,22 +42,18 @@ describe('console functions', function(){
   });
 
   it('prints the help string', function(){
-
-  	filename.call_help();
-
-  	let string_compared = hook.captured();
+    
+    filename.call_help();
+    let string_compared = hook.captured();
     string_compared = string_compared.split('\n').join('');
 
-  	assert.equal(string_compared, "Usage: grep [OPTION]... PATTERN [FILE]...Try 'grep --help' for more information.");
+    assert.equal(string_compared, "Usage: grep [OPTION]... PATTERN [FILE]...Try 'grep --help' for more information.");
   });
 
   it('printing to console', function(){
-
-  	filename.console_print("print this to console");
-    
+    filename.console_print("print this to console");
     assert.equal(hook.captured(),"print this to console\n");
   });
-  
 
 });
 
@@ -69,12 +65,10 @@ describe('preparing the content:', function(){
   let flags_value = {}
 
   it('fetch file name', function(){
-
     string_print = { 'filename' : [] };
     let file = 'package.json';
-
-  	filename.prepare_string_print('filename', string_print, tab_flag, flags_value, 0, {}, {}, file);
-  	assert.equal(string_print['filename'], file +'-');
+    filename.prepare_string_print('filename', string_print, tab_flag, flags_value, 0, {}, {}, file);
+    assert.equal(string_print['filename'], file +'-');
 
   });
 
@@ -83,8 +77,8 @@ describe('preparing the content:', function(){
     string_print = { 'lineno' : [] };
     flags_value['index'] = true;
     let current_offset = 100;
-  	filename.prepare_string_print('lineno', string_print, tab_flag, flags_value, current_offset, {}, {}, 'file');
-  	assert.equal(string_print['lineno'], '\t'+Number(current_offset).toString()+"-\t");
+    filename.prepare_string_print('lineno', string_print, tab_flag, flags_value, current_offset, {}, {}, 'file');
+    assert.equal(string_print['lineno'], '\t'+Number(current_offset).toString()+"-\t");
     
 
     string_print = { 'lineno' : [] };
@@ -99,13 +93,93 @@ describe('preparing the content:', function(){
 
   });
 
+
+  it('fetch index value', function(){
+    
+    string_print = { 'index' : [] };
+    tab_flag = 1;
+    flags_value['index'] = true;
+    let indexes_for_lines = {};
+    current_offset = 100;
+    indexes_for_lines[current_offset] = 'abcd';
+
+    filename.prepare_string_print('index', string_print, tab_flag, flags_value, current_offset, indexes_for_lines, {}, 'file');
+    assert.equal(string_print['index'], indexes_for_lines[current_offset]+"\t-");
+    
+
+    tab_flag = 0;
+    string_print = { 'index' : [] };
+    filename.prepare_string_print('index', string_print, tab_flag, flags_value, current_offset, indexes_for_lines, {}, 'file');
+    assert.equal(string_print['index'], indexes_for_lines[current_offset]+"-");
+  });
+
+
   it('fetch the line content', function(){
-  	let lineno = 10;
-  	let lines = { '10': "testing line !!" };
+    let lineno = 10;
+    let lines = { '10': "testing line !!" };
     string_print = { 'line': [] };
 
     filename.prepare_string_print('line', string_print, tab_flag, flags_value, lineno+1, {}, lines, 'file');
     assert.equal(string_print['line'], lines[lineno]);
   });  
+
+});
+
+describe('content iteration', function(){
+  
+  let hook;
+  beforeEach(function(){
+    hook = captureStream(process.stdout);
+  });
+
+  afterEach(function(){
+    hook.unhook(); 
+  });
+
+  let flags_value = {};
+  let file = 'anything.js';
+  let structure = {};
+  structure['1'] = { 'line' : 'testing line 1' };
+  structure['2'] = { 'line' : 'testing line 2' };
+
+  it('-c count option', function(){
+    
+    flags_value['count'] = true;
+    
+    filename.loop_over_content(structure, file, flags_value, {}, 0, {}, {});
+    assert.equal(hook.captured(), `${file} : ${Object.keys(structure).length}\n`);
+  });
+
+});
+
+describe('update content', function(){
+  
+  let anymatch = 0;
+  let file = 'anything.js';
+  let line_number = 1;
+  let lines = { '1': "google rocks" }
+  let pattern = "google";
+  let matched_result = "google";
+
+  let matched_content = { 'file': 'anything.js' };
+  matched_content[file] = { 'matched': {}, 'unmatched': {} };
+
+  it('for matched - updation of line, index, matched_value:', function(){
+    anymatch = 0;
+    filename.update_content(anymatch, matched_content, file, line_number+1, lines, pattern, matched_result);
+    assert.equal(matched_content[file]['matched'][line_number+1]['line'], lines[line_number] );
+  });
+
+  it('for matched - updation of matched_value:', function(){
+    anymatch = 0;
+    filename.update_content(anymatch, matched_content, file, line_number+1, lines, pattern, matched_result);
+    assert.equal(matched_content[file]['matched'][line_number+1]['match_value'], pattern);
+  });
+
+  it('for matched - updation of index:', function(){
+    anymatch = 0;
+    filename.update_content(anymatch, matched_content, file, line_number+1, lines, pattern, matched_result);
+    assert.equal(matched_content[file]['matched'][line_number+1]['indexes'], matched_result);
+  });
 
 });
